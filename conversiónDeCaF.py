dc31a00d71f85a8d1e37c7503566044d6e5dcff3
CamiloStiwar <camilo.cm96@gmail.com>
2023-03-02 10:08:24 -0500

    convertir grados fahrenheit

---
+++ conversiónDeCaF.py
@@ -0,0 +1,3 @@
+valorGradosCentigrados = int(input("Ingrese el valor de los grados °C: "))
+valorGradosFahrenheit = (valorGradosCentigrados * 9/5) + 32
+print(f"El valor de la conversión de °c a fahrenheit es {valorGradosFahrenheit}")
